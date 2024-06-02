7d08866b7b8555ea5aa7d5632cc3876b46f05cbd
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:31:52 -0500

    improve

--- pilot/database/models/environment_setup.py
+++ pilot/database/models/environment_setup.py
@@ -1,6 +1,13 @@
 from database.models.components.progress_step import ProgressStep
 
-
 class EnvironmentSetup(ProgressStep):
     class Meta:
-        table_name = 'environment_setup'
+        db_table = 'environment_setup'
+        primary_key = True
+
+
+from database.models.components import ProgressStep
+
+class EnvironmentSetup(ProgressStep):
+    db_table = 'environment_setup'
+    primary_key = True
