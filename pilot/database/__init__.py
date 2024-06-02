7fafc13d7e0dae5e190dbba564943d38d1578f25
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:31:16 -0500

    improve

--- pilot/database/__init__.py
+++ pilot/database/__init__.py
@@ -1,1 +1,12 @@
-from .database import database_exists, create_database, save_app
+from .database import database\_exists, create\_database, save\_app
+
+def establish\_database\_connection():
+    if not database\_exists():
+        create\_database()
+    # any additional setup or connection code can be added here
+
+# optionally, you can also include a function to close the database connection
+def close\_database\_connection():
+    # code to close the database connection goes here
+    pass
+
