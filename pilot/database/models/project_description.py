27ae805d6aa7c174789c31d48ef8ba40dcb72ab8
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:32:08 -0500

    improve

--- pilot/database/models/project_description.py
+++ pilot/database/models/project_description.py
@@ -1,6 +1,8 @@
 from peewee import TextField
+from playhouse.migrate import SqliteDatabase, migrate
 from database.models.components.progress_step import ProgressStep
 
+db = SqliteDatabase('path/to/your/database.db')
 
 class ProjectDescription(ProgressStep):
     prompt = TextField()
@@ -8,3 +10,14 @@ class ProjectDescription(ProgressStep):
 
     class Meta:
         table_name = 'project_description'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.table_name = 'project_description'
+
+def create_table():
+    with db:
+        db.create_tables([ProjectDescription])
+
+if __name__ == '__main__':
+    create_table()
