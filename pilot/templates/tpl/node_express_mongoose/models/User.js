2a8de85d7877144aefbcb5191a1c7f63f0b73fcb
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:32:46 -0500

    improve

--- pilot/templates/tpl/node_express_mongoose/models/User.js
+++ pilot/templates/tpl/node_express_mongoose/models/User.js
@@ -2,22 +2,49 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  password: { type: String, required: true }
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    minlength: 3,
+    maxlength: 32
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 1024
+  },
+  passwordConfirmation: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(value) {
+        return value === this.password;
+      },
+      message: 'Passwords do not match'
+    }
+  }
 });
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', async function(next) {
   const user = this;
   if (!user.isModified('password')) return next();
-  bcrypt.hash(user.password, 10, (err, hash) => {
-    if (err) {
-      console.error('Error hashing password:', err);
-      return next(err);
-    }
+  const saltRounds = 10;
+  try {
+    const hash = await bcrypt.hash(user.password, saltRounds);
     user.password = hash;
+    user.passwordConfirmation = undefined;
     next();
-  });
+  } catch (err) {
+    console.error('Error hashing password:', err);
+    return next(err);
+  }
 });
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
