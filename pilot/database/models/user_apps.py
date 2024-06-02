6dfa8fe719f2c6ebd460996fa0862185d820d0c3
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:32:23 -0500

    improve

--- pilot/database/models/user_apps.py
+++ pilot/database/models/user_apps.py
@@ -4,8 +4,7 @@ from database.models.components.base_models import BaseModel
 from database.models.app import App
 from database.models.user import User
 
-
-class UserApps(BaseModel):
+class UserApp(BaseModel):
     id = AutoField()
     app = ForeignKeyField(App, on_delete='CASCADE')
     user = ForeignKeyField(User, on_delete='CASCADE')
@@ -15,4 +14,9 @@ class UserApps(BaseModel):
         table_name = 'user_apps'
         indexes = (
             (('app', 'user'), True),
+            (('user', 'app'), True),  # Added this index for better performance when querying by user and app
         )
+
+    def __str__(self):
+        return f"<UserApp: {self.app.name} by {self.user.username} in {self.workspace}>"
+
