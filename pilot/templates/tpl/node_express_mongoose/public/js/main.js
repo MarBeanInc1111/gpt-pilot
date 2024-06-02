772868f773c68e9328edc33e57f614a6819969fc
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:32:15 -0500

    improve

--- pilot/templates/tpl/node_express_mongoose/public/js/main.js
+++ pilot/templates/tpl/node_express_mongoose/public/js/main.js
@@ -1,1 +1,18 @@
-// Placeholder for future JavaScript code
+// A simple JavaScript function that takes a number as input and returns its double
+function double(number) {
+  return number * 2;
+}
+
+// A JavaScript function that takes an array of numbers as input and returns the sum of all the numbers
+function sumArray(numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+}
+
+// An example usage of the above functions
+const result = double(sumArray([1, 2, 3, 4]));
+console.log(result); // Output: 20
+
