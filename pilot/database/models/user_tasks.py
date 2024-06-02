4c850782e81683a7a47ac14f98826afbed52a520
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:31:42 -0500

    improve

--- pilot/database/models/user_tasks.py
+++ pilot/database/models/user_tasks.py
@@ -6,9 +6,19 @@ from playhouse.postgres_ext import BinaryJSONField
 
 class UserTasks(ProgressStep):
     if DATABASE_TYPE == 'postgres':
-        user_tasks = BinaryJSONField()
+        user_tasks = BinaryJSONField(index=True)
     else:
         user_tasks = JSONField()  # Custom JSON field for SQLite
 
     class Meta:
         table_name = 'user_tasks'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.user_tasks is None:
+            self.user_tasks = {}
+
+    def save(self, *args, **kwargs):
+        if self.user_tasks is None:
+            self.user_tasks = {}
+        return super().save(*args, **kwargs)
