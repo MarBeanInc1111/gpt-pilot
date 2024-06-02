69701e74c9f31ed31917548f4cd87a2f5c240cdb
MarBeanInc1111 <142996448+MarBeanInc1111@users.noreply.github.com>
2024-06-01 22:31:56 -0500

    improve

--- pilot/test/mock_questionary.py
+++ pilot/test/mock_questionary.py
@@ -1,15 +1,28 @@
 class MockQuestionary:
-    def __init__(self, answers=None, initial_state='project_description'):
+    """
+    A mock questionary class for testing.
+    """
+
+    def __init__(self, answers: list = None, initial_state: str = 'project_description'):
+        """
+        Initialize the MockQuestionary object.
+
+        :param answers: A list of strings representing the user's answers
+        :param initial_state: The initial state of the questionary
+        """
         if answers is None:
             answers = []
         self.answers = iter(answers)
         self.state = initial_state
 
-    class Style:
-        def __init__(self, *args, **kwargs):
-            pass
-
     def text(self, question: str, style=None):
+        """
+        Print the given question and update the state accordingly.
+
+        :param question: The question to print
+        :param style: The style of the question
+        :return: The MockQuestionary object
+        """
         print('AI: ' + question)
         if question.startswith('User Story'):
             self.state = 'user_stories'
@@ -17,16 +30,29 @@ class MockQuestionary:
             self.state = 'DONE'
         return self
 
-    def ask(self):
-        return self.unsafe_ask()
+    def ask(self) -> str:
+        """
+        Ask the user for an answer and return it.
 
-    def unsafe_ask(self):
+        :return: The user's answer
+        """
+        return self._ask()
+
+    def _ask(self) -> str:
+        """
+        Implementation detail of the `ask` method.
+
+        :return: The user's answer
+        """
         if self.state == 'user_stories':
             answer = ''
         elif self.state == 'DONE':
             answer = 'DONE'
         else:  # if self.state == 'project_description':
-            answer = next(self.answers, '')
+            try:
+                answer = next(self.answers)
+            except StopIteration:
+                answer = ''
 
         print('User:', answer)
         return answer
